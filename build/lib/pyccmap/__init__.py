64a8e4570989a42c7e9e82f58e8a509d14095e84
Wanderson Gonçalves Nunes <wanderson@sentinella.com.br>
2019-11-01 14:15:22 -0300

    Cleanup

--- build/lib/pyccmap/__init__.py
+++
@@ -1,3 +0,0 @@
-from .api import API
-from .apis import Cex, BitRex, BitStamp
-from .directory import Directory
--- build/lib/pyccmap/apis/__init__.py
+++
@@ -1,5 +0,0 @@
-from .bitrex import BitRex
-from .bitstamp import BitStamp
-from .cex import Cex
-
-__all__ = ["BitRex", "BitStamp", "Cex"]
