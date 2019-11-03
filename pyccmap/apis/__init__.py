6cf34ce6d60fe9ee1bc407530e2b70854df98cbb
Wanderson Gonçalves Nunes <wanderson@sentinella.com.br>
2019-11-03 20:11:41 -0300

    Bugfixes and added BitFinex support.

--- pyccmap/apis/__init__.py
+++ pyccmap/apis/__init__.py
@@ -1,5 +1,6 @@
 from .bitrex import BitRex
 from .bitstamp import BitStamp
 from .cex import Cex
+from .bitfinex import BitFinex
 
-__all__ = ["BitRex", "BitStamp", "Cex"]
+__all__ = ["BitRex", "BitStamp", "Cex", "BitFinex"]
